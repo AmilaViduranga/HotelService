c16217df1d0632f6e4ee5b965255061f82d4524e
AmilaViduranga <amila@info-share.org>
2017-03-29 01:32:46 +0530

    initial commit

---
+++ Server/app/Controller/ControllerMap.js
@@ -0,0 +1,11 @@
+/**
+ * Created by User on 9/9/2016.
+ * developer: -Kasun
+ * use as map for all the controllers allocating
+ */
+var Controllers = {};
+
+Controllers.HotelController = require('./ModuleControllers/HotelController');
+Controllers.RoomsController = require('./ModuleControllers/RoomsController');
+
+module.exports = Controllers;
